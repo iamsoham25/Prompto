bc3b7ab26ecc92463fc3a45ecde5a85b99f0cd30
iamsoham25 <sohamthoke@gmail.com>
2026-06-29 11:30:02 +0530

    Worked on UI Card page

--- frontend/src/components/ui/Card.tsx
+++ frontend/src/components/ui/Card.tsx
@@ -11,19 +11,7 @@ export default function Card({
 }: CardProps) {
   return (
     <div
-      className="
-      bg-white
-      border border-slate-200
-      rounded-3xl
-      p-6
-      shadow-md
-      hover:shadow-xl
-      hover:border-orange-400
-      hover:scale-105
-      transition-all
-      duration-300
-      cursor-pointer
-      "
+      className=" bg-white border border-slate-200 rounded-3xl p-6 shadow-md hover:shadow-xl hover:border-orange-400 hover:scale-105 transition-all duration-300 cursor-pointer "
     >
       <h2 className="text-slate-500 text-sm uppercase tracking-wider mb-3">
         {title}
