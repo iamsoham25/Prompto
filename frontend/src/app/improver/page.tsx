14b7377107ed7e819c9a11087daacd8c4176cb95
iamsoham25 <sohamthoke@gmail.com>
2026-07-07 16:26:10 +0530

    Working on improver Page

--- frontend/src/app/improver/page.tsx
+++ frontend/src/app/improver/page.tsx
@@ -179,15 +179,7 @@ export default function ImproverPage() {
         ========================== */}
 
         <div
-          className="
-          bg-white
-          rounded-3xl
-          p-8
-          shadow-lg
-          border
-          border-slate-200
-          "
-        >
+          className=" bg-white rounded-3xl p-8 shadow-lg border border-slate-200 " >
 
           <h2 className="text-2xl font-bold mb-4">
 
