8f18e14e18b078866d5ec24aab5a2cff97708dd1
iamsoham25 <sohamthoke@gmail.com>
2026-07-31 15:36:14 +0530

    Working on Analysis Page

--- frontend/src/components/analytics/SkillBarChart.tsx
+++ frontend/src/components/analytics/SkillBarChart.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-    import{ ResponsiveContainer, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip }from"recharts";
+import{ ResponsiveContainer, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip }from"recharts";
 
 export default function SkillBarChart({
 
-data
+    data
 
-}:{
+    }:{
 
-data:any[];
+    data:any[];
 
 }){
 
