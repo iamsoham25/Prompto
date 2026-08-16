d72b52e5ed2b24d8ce0f2b8cc5888220db124032
iamsoham25 <sohamthoke@gmail.com>
2026-08-16 10:22:09 +0530

    Working on Home page

--- frontend/src/components/home/ChallengesShowcase.tsx
+++ frontend/src/components/home/ChallengesShowcase.tsx
@@ -120,11 +120,7 @@ export default function ChallengesShowcase() {
     return (
     <section
       id="challenges-showcase"
-      className="
-        relative
-        overflow-hidden
-        bg-[#080910]
-        py-24
+      className=" relative overflow-hidden bg-[#080910] py-24
         text-white
 
         sm:py-28
