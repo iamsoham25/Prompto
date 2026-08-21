f1358057bb5c196fe213d357982a36941a80606a
iamsoham25 <sohamthoke@gmail.com>
2026-08-21 13:41:12 +0530

    Updating Home Page UI

--- frontend/src/components/home/AnalyticsShowcase.tsx
+++ frontend/src/components/home/AnalyticsShowcase.tsx
@@ -242,13 +242,7 @@ export default function AnalyticsShowcase() {
             </div>
 
             <div
-              className=" flex w-fit items-center gap-2 rounded-full bg-emerald-50
-                px-3
-                py-2
-                text-xs
-                font-bold
-                text-emerald-700
-              "
+              className=" flex w-fit items-center gap-2 rounded-full bg-emerald-50 px-3 py-2 text-xs font-bold text-emerald-700 "
             >
               <span
                 className="
