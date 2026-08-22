7346e5ebe4cbfda7f99da1807679804d170684c4
iamsoham25 <sohamthoke@gmail.com>
2026-08-22 20:25:30 +0530

    Updating Home Page UI

--- frontend/src/components/home/AnalyticsShowcase.tsx
+++ frontend/src/components/home/AnalyticsShowcase.tsx
@@ -302,11 +302,7 @@ export default function AnalyticsShowcase() {
               {/* TREND CHART */}
 
               <div
-                className=" min-w-0 rounded-[26px] border border-slate-200 bg-slate-50/70
-                  p-5
-
-                  sm:p-6
-                "
+                className=" min-w-0 rounded-[26px] border border-slate-200 bg-slate-50/70 p-5 sm:p-6 "
               >
                 <div
                   className="
