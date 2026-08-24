f65d01ce2595b39d56715cabd0923f903314e1cd
iamsoham25 <sohamthoke@gmail.com>
2026-08-24 11:05:32 +0530

    Upgrading Home Page

--- frontend/src/components/home/AnalyticsShowcase.tsx
+++ frontend/src/components/home/AnalyticsShowcase.tsx
@@ -309,13 +309,7 @@ export default function AnalyticsShowcase() {
                 >
                   <div>
                     <p
-                      className="
-                        text-xs
-                        font-black
-                        uppercase
-                        tracking-[0.16em]
-                        text-slate-400
-                      "
+                      className=" text-xs font-black uppercase tracking-[0.16em] text-slate-400 "
                     >
                       Prompt Performance
                     </p>
