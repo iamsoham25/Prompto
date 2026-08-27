c787a8325f63225676ea1a8b92d2281ef619f5bf
iamsoham25 <sohamthoke@gmail.com>
2026-08-27 22:50:32 +0530

    Upgrading Learn page

--- frontend/src/components/home/AnalyticsShowcase.tsx
+++ frontend/src/components/home/AnalyticsShowcase.tsx
@@ -430,9 +430,7 @@ export default function AnalyticsShowcase() {
                 </p>
 
                 <h4
-                  className=" mt-1
-                    text-xl
-                    font-black
+                  className=" mt-1 text-xl font-black
                     text-slate-950
                   "
                 >
