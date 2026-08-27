81a89f448b7b56fcd5dcda7cd84bd66acc943dc5
iamsoham25 <sohamthoke@gmail.com>
2026-08-27 22:54:10 +0530

    Upgrading Home page

--- frontend/src/components/home/AnalyticsShowcase.tsx
+++ frontend/src/components/home/AnalyticsShowcase.tsx
@@ -470,10 +470,7 @@ export default function AnalyticsShowcase() {
                 </div>
 
                 <div
-                  className=" flex items-center justify-center gap-2 text-xs
-                    font-semibold
-                    text-slate-500
-                  "
+                  className=" flex items-center justify-center gap-2 text-xs font-semibold text-slate-500 "
                 >
                   <span
                     className="
