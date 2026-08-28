3be884d4f4600ee8689751b15bc6bb34ebdc331f
iamsoham25 <sohamthoke@gmail.com>
2026-08-28 23:38:17 +0530

    Upgrading Home page

--- frontend/src/components/home/AnalyticsShowcase.tsx
+++ frontend/src/components/home/AnalyticsShowcase.tsx
@@ -491,8 +491,7 @@ export default function AnalyticsShowcase() {
               {/* Skill breakdown */}
 
               <div
-                className=" rounded-[26px] border border-slate-200
-                  p-5
+                className=" rounded-[26px] border border-slate-200 p-5
 
                   sm:p-6
                 "
