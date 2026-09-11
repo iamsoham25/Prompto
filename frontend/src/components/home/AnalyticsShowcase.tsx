0cd9955ab9f7e78c5959f0c22f664425e2e7d5cd
iamsoham25 <sohamthoke@gmail.com>
2026-09-11 12:38:43 +0530

    Changing Home Page

--- frontend/src/components/home/AnalyticsShowcase.tsx
+++ frontend/src/components/home/AnalyticsShowcase.tsx
@@ -523,14 +523,7 @@ export default function AnalyticsShowcase() {
               {/* AI Insights */}
 
               <div
-                className="
-                  rounded-[26px]
-                  bg-slate-950
-                  p-5
-                  text-white
-
-                  sm:p-6
-                "
+                className=" rounded-[26px] bg-slate-950 p-5 text-white sm:p-6 "
               >
                 <div
                   className="
