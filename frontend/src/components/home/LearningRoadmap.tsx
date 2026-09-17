38b39d2d541a3c27476a0f34f5f9e2a76f494d7c
iamsoham25 <sohamthoke@gmail.com>
2026-09-17 16:28:14 +0530

    Changing home page

--- frontend/src/components/home/LearningRoadmap.tsx
+++ frontend/src/components/home/LearningRoadmap.tsx
@@ -792,8 +792,7 @@ function RoadmapContent({
       >
         <span
           className={`
-            text-xs
-            font-black uppercase tracking-[0.18em]
+            text-xs font-black uppercase tracking-[0.18em]
 
             ${theme.text}
           `}
