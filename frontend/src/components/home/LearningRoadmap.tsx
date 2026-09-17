c46c94f4c236e94308e642f9b65e389d7a752f48
iamsoham25 <sohamthoke@gmail.com>
2026-09-17 16:27:27 +0530

    Changing home page

--- frontend/src/components/home/LearningRoadmap.tsx
+++ frontend/src/components/home/LearningRoadmap.tsx
@@ -804,8 +804,7 @@ function RoadmapContent({
         </span>
 
         <span
-          className={`
-            rounded-full px-3 py-1 text-xs font-bold
+          className={` rounded-full px-3 py-1 text-xs font-bold
 
             ${theme.soft}
             ${theme.text}
