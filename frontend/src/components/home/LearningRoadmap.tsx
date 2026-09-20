25d550b3dfb084ea6416e0bce42ec25b4f12d9cd
iamsoham25 <sohamthoke@gmail.com>
2026-09-20 22:59:31 +0530

    Changing home page

--- frontend/src/components/home/LearningRoadmap.tsx
+++ frontend/src/components/home/LearningRoadmap.tsx
@@ -732,8 +732,7 @@ export default function LearningRoadmap() {
                 shadow-lg
                 transition-all
                 duration-300
-
-     hover:-translate-y-1 hover:bg-orange-600 hover:shadow-orange-500/20 "
+ hover:-translate-y-1 hover:bg-orange-600 hover:shadow-orange-500/20 "
             >
               Explore Learning
 
