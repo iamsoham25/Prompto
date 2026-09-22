66b2b853b00139aa6fc5a62cfb60142077910c6f
iamsoham25 <sohamthoke@gmail.com>
2026-09-22 19:17:32 +0530

    Changing home page

--- frontend/src/components/home/WhatIsPrompto.tsx
+++ frontend/src/components/home/WhatIsPrompto.tsx
@@ -245,8 +245,7 @@ export default function WhatIsPrompto() {
                       text-slate-800
                       transition
                       duration-200
-
-       group-hover:-translate-y-1 group-hover:bg-orange-500 group-hover:text-white "
+ group-hover:-translate-y-1 group-hover:bg-orange-500 group-hover:text-white "
                   >
                     <Icon size={22} />
                   </div>
