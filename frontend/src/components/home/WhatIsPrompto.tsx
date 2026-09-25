f1522beca6fad5d2c52d5375c5641c34d0e0987d
iamsoham25 <sohamthoke@gmail.com>
2026-09-25 15:20:31 +0530

    Changing Home Page

--- frontend/src/components/home/WhatIsPrompto.tsx
+++ frontend/src/components/home/WhatIsPrompto.tsx
@@ -238,7 +238,7 @@ export default function WhatIsPrompto() {
 
                   <div
                     className=" flex h-12 w-12
-       items-center justify-center
+ items-center justify-center
                       rounded-2xl
                       bg-slate-100
                       text-slate-800
